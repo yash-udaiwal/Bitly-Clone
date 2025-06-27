9e69c9b051e99f4f7ecb11cb95ca1f9f8f7b1e7b
Yash Udaiwal <yashudaiwal2@gmail.com>
2025-06-27 23:13:27 +0530

    Initial commit

---
+++ src/test/java/com/url/url/shortener/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.url.url.shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
